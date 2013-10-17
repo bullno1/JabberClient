2db0086af819f89025196fb570e1c5b90e6c703f
bullno1 <thebullno1@gmail.com>
2013-10-17 22:40:16 +0800

    Implement sending of chat log

--- src/cs3013/plugins/ChatPlugin.java
+++ src/cs3013/plugins/ChatPlugin.java
@@ -1,5 +1,9 @@
 package cs3013.plugins;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
@@ -18,6 +22,7 @@ public class ChatPlugin implements Plugin, ChatHandler, StanzaHandler {
 	public void init(JabberClient client) {
 		client.registerCommand("chat", new ChatCommand());
 		client.registerCommand("end", new EndCommand());
+		client.registerCommand("log", new LogCommand());
 		client.registerStanzaHandler(this);
 		client.setChatHandler(this);
 		
@@ -35,6 +40,7 @@ public class ChatPlugin implements Plugin, ChatHandler, StanzaHandler {
 		}
 		else {
 			sendMessage(currentFriendJID, msg);
+			chatLog.append("me: ").append(msg).append("\n");
 		}
 	}
 
@@ -48,7 +54,11 @@ public class ChatPlugin implements Plugin, ChatHandler, StanzaHandler {
 			case XMLStreamConstants.START_ELEMENT:
 				String tagName = parser.getLocalName();
 				if(tagName.equals("body")) {
-					client.print(friendJID + " says: " + parser.getElementText());
+					String msg = parser.getElementText();
+					client.print(friendJID + " says: " + msg);
+					if(friendJID.equals(currentFriendJID)) {
+						chatLog.append(friendJID).append(": ").append(msg).append("\n");
+					}
 				}
 				else {
 					XMLUtils.skipElement(parser);
@@ -73,9 +83,98 @@ public class ChatPlugin implements Plugin, ChatHandler, StanzaHandler {
 
 		client.send(stanza);
 	}
+	
+	private void startSession(String friendJID) {
+		if(currentFriendJID != null) {
+			endSession();
+		}
 
+		currentFriendJID = friendJID;
+		chatLog.setLength(0);
+	}
+	
+	private void endSession() {
+		if(currentFriendJID == null) return;
+
+		if(logEnabled) sendChatLog();
+
+		currentFriendJID = null;
+	}
+	
+	private void sendChatLog() {
+		client.print("Sending chat log");
+
+		try(Socket socket = new Socket()) {
+			socket.connect(new InetSocketAddress(logServerAddress, logServerPort));
+			try(DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+				//friend jid
+				output.writeUTF(currentFriendJID);
+				//size
+				byte[] log = chatLog.toString().getBytes("UTF-8");
+				output.writeInt(log.length);
+				output.write(log);
+			}
+		} catch (IOException e) {
+			client.print("Failed to send log");
+			e.printStackTrace();
+		}
+		client.print("Done");
+	}
+
+	StringBuilder chatLog = new StringBuilder();
+	String logServerAddress = "127.0.0.1";
+	int logServerPort = 9001;
+	boolean logEnabled = true;
 	String currentFriendJID;
 	JabberClient client;
+
+	private class LogCommand implements Command {
+
+		@Override
+		public String getShortDescription() {
+			return "View/set log settings";
+		}
+
+		@Override
+		public String getLongDescription() {
+			return "@log - view current log setting.\n"
+			      +"@log on - enable logging (enabled by default).\n"
+				  +"@log on <address> <port> - enable logging and change server settings.\n"
+			      +"@log off - disable logging.";
+		}
+
+		@Override
+		public void execute(String[] args) throws InterruptedException {
+			switch(args.length) {
+			case 1://view settings
+				if(logEnabled) {
+					client.print("Logging is on");
+					client.print("Log server: " + logServerAddress);
+					client.print("Port: " + logServerPort);
+				}
+				else {
+					client.print("Logging is off");
+				}
+				break;
+			case 2://enable/disable
+				logEnabled = args[1].equals("on");
+				break;
+			case 4:
+				try {
+					logServerPort = Integer.parseInt(args[3]);
+					logEnabled = args[1].equals("on");
+					logServerAddress = args[2];
+				}
+				catch(NumberFormatException e) {
+					client.print("Syntax error");
+				}
+				break;
+			default:
+				client.print("Syntax error");
+				break;
+			}
+		}
+	}
 	
 	private class ChatCommand implements Command {
 		@Override
@@ -93,7 +192,7 @@ public class ChatPlugin implements Plugin, ChatHandler, StanzaHandler {
 		@Override
 		public void execute(String[] args) {
 			if(args.length == 2) {
-				currentFriendJID = args[1];
+				startSession(args[1]);
 			}
 			else {
 				client.print("Syntax error");
@@ -114,7 +213,7 @@ public class ChatPlugin implements Plugin, ChatHandler, StanzaHandler {
 
 		@Override
 		public void execute(String[] args) {
-			currentFriendJID = null;
+			endSession();
 		}
 	}
 }
